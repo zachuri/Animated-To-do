7095585de9d8fa92dacbd502fe0ef2a18e3cca30
zachuri <punsalang.zachary@gmail.com>
2022-06-04 23:00:50 -0700

    adding dark theme support/ almost complete

--- App.tsx
+++ App.tsx
@@ -1,13 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import AppContainer from './src/components/app-container'
+import Main from './src/screens/main'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <AppContainer>
+      <Main></Main>
+    </AppContainer>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -15,6 +17,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: 'center'
+  }
+})
---
+++ src/screens/main.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react'
+import {
+  Text,
+  Box,
+  Center,
+  VStack,
+  themeTools,
+  useTheme,
+  useColorMode,
+  useColorModeValue
+} from 'native-base'
+import ThemeToggle from '../components/theme-toggle'
+
+export default function MainScreen() {
+  return (
+    <Center
+      _dark={{ bg: 'blueGray.900' }}
+      _light={{ bg: 'blueGray.50' }}
+      px={4}
+      flex={1}
+    >
+      <VStack space={5} alignItems="center">
+        <Box>
+          <Text>Hello</Text>
+        </Box>
+        <ThemeToggle /></ThemeToggle>
+        <ThemeToggle />
+      </VStack>
+    </Center>
+  )
+}
